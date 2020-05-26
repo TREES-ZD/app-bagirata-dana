14ff948e7a747da18869811ef7206fb1612097c7
Janick Norman <janicknorman@gmail.com>
2020-05-26 17:22:13 +0700

    Add matchAssignments in Task model

--- app/Task.php
+++ app/Task.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
@@ -11,5 +12,31 @@ class Task extends Model
     public function rules()
     {
         return $this->belongsToMany('App\Agent', 'rules');
+    }
+
+    public function matchAssignments(Collection $tickets) {
+        $agents = $this->rules()
+                        ->disableCache()
+                        ->where('status', true)
+                        ->with(['assignments'])
+                        ->get()
+                        ->sortBy(function($a) {
+                            return $a->assignments->last() ? $a->assignments->last()->created_at->timestamp : 0;
+                        });              
+        
+        if ($agents->count() < 1) {
+            return collect();
+        } 
+        $totalAgents = $agents->count();
+
+        $match = $tickets->map(function($ticket, $index) use ($agents) {
+            $agentNum = ($index % $agents->count());
+            $agent = $agents->get($agentNum);
+            return collect([
+                "agent" => $agent,
+                "ticket" => $ticket
+            ]);
+        });
+        return $match;
     }
 }
