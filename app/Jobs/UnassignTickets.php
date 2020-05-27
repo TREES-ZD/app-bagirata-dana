0a6cc5eeb2f43ac59f98e8d1dfd63bc05db1ea73
Janick Norman <janicknorman@gmail.com>
2020-05-27 12:54:36 +0700

    Refactor UnassignTIcketsTask

--- app/Jobs/UnassignTickets.php
+++ app/Jobs/UnassignTickets.php
@@ -5,18 +5,22 @@ namespace App\Jobs;
 use App\Agent;
 use App\Assignment;
 use App\AvailabilityLog;
+use App\Services\ZendeskService;
+use Illuminate\Support\Str;
 use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Zendesk\API\HttpClient as ZendeskAPI;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Support\Facades\Log;
 use Zendesk\API\Exceptions\ApiResponseException;
 
 class UnassignTickets implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $agent;
 
     /**
      * Create a new job instance.
@@ -33,47 +37,25 @@ class UnassignTickets implements ShouldQueue
      *
      * @return void
      */
-    public function handle()
+    public function handle(ZendeskService $zendesk)
     {
-        $subdomain = env("ZENDESK_SUBDOMAIN", "contreesdemo11557827937");
-        $username  = env("ZENDESK_USERNAME", "eldien.hasmanto@treessolutions.com");
-        $token     = env("ZENDESK_TOKEN", "2HJtvL35BSsWsVR4b3ZCxvYhLGYcAacP2EyFKGki"); // replace this with your token
-        
-        $client = new ZendeskAPI($subdomain);
-        $client->setAuth('basic', ['username' => $username, 'token' => $token]);
-
         $agent_id = $this->agent->id;
         $latest_log = AvailabilityLog::where("agent_id", $agent_id)->where("status", "Available")->latest()->first(); //TODO: check if not exist
-        $ticket_ids = Assignment::where('agent_id', $agent_id)->where('type', Agent::ASSIGNMENT)->whereDate('created_at', ">=", $latest_log->created_at)->get()->pluck('ticket_id');
+        $assignments_builder = Assignment::where('agent_id', $agent_id)->where('type', Agent::ASSIGNMENT)->whereDate('created_at', ">=", $latest_log->created_at);
+        $ticket_ids = $assignments_builder->get()->pluck('ticket_id');
+        $view_by_ticket_id = $assignments_builder->get()->pluck('zendesk_view_id', 'ticket_id');
 
         $tickets = null;
         try {
-            $response = $client->tickets()->findMany($ticket_ids->toArray());
-            $tickets = $response->tickets;
+            $tickets = $zendesk->getTicketsByIds($ticket_ids->toArray());
         } catch (ApiResponseException $apiException) {
             Log::error($apiException);
             return;
         }
 
-        //Unassign
-        // $client->tickets()->updateMany([
-        //     "ids" => array_column($tickets, "id"),
-        //     "assignee_id" => null,
-        //     "group_id" => $this->agent->zendesk_group_id,
-        //     "custom_fields" => [
-        //         [
-        //         "id" => env("ZENDESK_AGENT_NAMES_FIELD", 360000282796),
-        //         "value" => null
-        //         ]
-        //     ],
-        //     "additional_tags" => ["bagirata_agent_unavailable"],
-        //     "comment" =>  [
-        //         "body" => "BagiRata Agent Unavailable: " . $this->agent->fullName,
-        //         "public" => false
-        //     ]
-        // ]);
+        $batch_id = (string) Str::uuid();
         foreach ($tickets as $i => $ticket) {
-            $client->tickets()->update($ticket->id, [
+            $zendesk->updateTicket($ticket->id, [
                 "custom_fields" => [
                     [
                     "id" => env("ZENDESK_AGENT_NAMES_FIELD", 360000282796),
@@ -89,10 +71,14 @@ class UnassignTickets implements ShouldQueue
             ]);
             $this->agent->assignments()->create([
                 "type" => Agent::UNASSIGNMENT,
+                "zendesk_view_id" => $view_by_ticket_id->get($ticket->id),
+                "batch_id" => $batch_id,
+                "agent_id" => $this->agent->id,
                 "agent_name" => $this->agent->fullName,
                 "ticket_id" => $ticket->id,
                 "ticket_name" => $ticket->subject,
-                "group_id" => $this->agent->zendesk_group_id
+                "group_id" => $this->agent->zendesk_group_id,
+                "response_status" => 200
             ]);
         }
 
