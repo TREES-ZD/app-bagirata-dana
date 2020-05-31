fe83cdab71d8d7a1fbb94c2e24111cfcdd89a7af
Janick Norman <janicknorman@gmail.com>
2020-05-31 13:20:10 +0700

    Add assignment factory

--- database/factories/AssignmentFactory.php
+++ database/factories/AssignmentFactory.php
@@ -2,11 +2,19 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Agent;
 use App\Assignment;
 use Faker\Generator as Faker;
 
 $factory->define(Assignment::class, function (Faker $faker) {
     return [
-        //
+        "type" => Agent::ASSIGNMENT,
+        "zendesk_view_id" => $faker->randomDigit,
+        "batch_id" => $faker->randomDigit,
+        "agent_id" => 1,
+        "agent_name" => "Andi",
+        "ticket_id" => 1,
+        "ticket_name" => "ticket_one",
+        "response_status" => "200"
     ];
 });
