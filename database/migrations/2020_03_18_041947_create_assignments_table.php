8c1797dab2c910da4e89807e9f7b202df3e47c6e
Janick Norman <janicknorman@gmail.com>
2020-06-02 00:29:56 +0700

    Change name migration

--- database/migrations/2020_03_18_041947_create_assignments_table.php
+++ database/migrations/2020_03_18_041947_create_assignments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAssignmentLogsTable extends Migration
+class CreateAssignmentTable extends Migration
 {
     /**
      * Run the migrations.
