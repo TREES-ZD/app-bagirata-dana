dfdf0d545c6865e098273cb19f2f9a19ef3408e3
Janick Norman <janicknorman@gmail.com>
2020-06-02 00:41:24 +0700

    add tasks table again

---
+++ database/migrations/2020_03_22_092213_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('zendesk_view_id');
+            $table->string('zendesk_view_title');
+            $table->integer('zendesk_view_position')->index();
+            $table->string('interval');
+            $table->string('group_id')->nullable();
+            $table->string('limit')->nullable();
+            $table->boolean('enabled');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
