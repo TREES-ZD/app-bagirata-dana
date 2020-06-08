60d58ab8df797795efba1ad3d0550996a04f2721
Janick Norman <janicknorman@gmail.com>
2020-06-08 12:08:26 +0700

    Not update when ticket is solved and closed

--- app/Jobs/UnassignTickets.php
+++ app/Jobs/UnassignTickets.php
@@ -52,22 +52,25 @@ class UnassignTickets implements ShouldQueue
         }
 
         foreach ($tickets as $i => $ticket) {
-            $zendesk->updateTicket($ticket->id, [
-                "custom_fields" => [
-                    [
-                    "id" => env("ZENDESK_AGENT_NAMES_FIELD", 360000282796),
-                    "value" => null
+            if (!in_array($ticket->status, ["solved", "closed"])) {
+                $zendesk->updateTicket($ticket->id, [
+                    "custom_fields" => [
+                        [
+                        "id" => env("ZENDESK_AGENT_NAMES_FIELD", 360000282796),
+                        "value" => null
+                        ]
+                    ],
+                    "tags" => array_merge($ticket->tags, ["bagirata_agent_unavailable"]),
+                    "comment" =>  [
+                        "body" => "BAGIRATA Agent Unavailable: " . $this->agent->fullName,
+                        "author_id" => $this->agent->zendesk_agent_id,
+                        "public" => false
                     ]
-                ],
-                "tags" => array_merge($ticket->tags, ["bagirata_agent_unavailable"]),
-                "comment" =>  [
-                    "body" => "BAGIRATA Agent Unavailable: " . $this->agent->fullName,
-                    "author_id" => $this->agent->zendesk_agent_id,
-                    "public" => false
-                ]
-            ]);
+                ]);
+            }
+
             $this->agent->assignments()->create([
-                "type" => Agent::UNASSIGNMENT,
+                "type" => in_array($ticket->status, ["solved", "closed"]) ? "ALREADY_SOLVED" : Agent::UNASSIGNMENT,
                 "zendesk_view_id" => $unnasignedTicketsByTicketId->get($ticket->id)->zendesk_view_id,
                 "batch_id" => $unnasignedTicketsByTicketId->get($ticket->id)->batch_id,
                 "agent_id" => $this->agent->id,
