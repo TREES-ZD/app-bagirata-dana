967a72af55c7486f683ccbc8fb2baf00297a3004
Janick Norman <janicknorman@gmail.com>
2020-06-09 10:34:23 +0700

    FIX

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -23,10 +23,10 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
         // Horizon::routeMailNotificationsTo('example@example.com');
         // Horizon::routeSlackNotificationsTo('slack-webhook-url', '#channel');
 
-        // Horizon::night();
-        $this->app->singleton(JobRepository::class, function($app) {
-            return new RedisJobRepository($app['redis.connection']);
-        });
+        Horizon::night();
+        // $this->app->singleton(JobRepository::class, function($app) {
+        //     return new RedisJobRepository($app['redis.connection']);
+        // });
         
 
     }
