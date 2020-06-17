3709ce850a721b763e8aaff76071dcefa9df33a2
Janick Norman <janicknorman@gmail.com>
2020-06-17 14:35:42 +0700

    Change unassignment with bulk

--- app/Jobs/UnassignTickets.php
+++ app/Jobs/UnassignTickets.php
@@ -6,6 +6,7 @@ use App\Agent;
 use Exception;
 use App\Assignment;
 use App\AvailabilityLog;
+use App\Events\UnassignmentsProcessed;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use Illuminate\Bus\Queueable;
@@ -26,7 +27,7 @@ class UnassignTickets implements ShouldQueue
 
     private $agent;
 
-    public $timeout = 800;
+    public $timeout = 1200;
     
     /**
      * Create a new job instance.
@@ -45,52 +46,22 @@ class UnassignTickets implements ShouldQueue
      */
     public function handle(ZendeskService $zendesk)
     {
-        // $unnasignedTickets = $this->agent->getUnassignedTickets()->chunk(100);
-        $unassignedTicketIds = Redis::smembers('agent:'.$this->agent->id.':assignedTickets');
+        $assignedTicketIds = Redis::smembers(sprintf("agent:%s:assignedTickets", $this->agent->id));
+        collect($assignedTicketIds)->chunk(100)->each(function($ticketIds) use ($zendesk) {
+            $tickets = $zendesk->getTicketsByIds($ticketIds->values()->all());
 
-        collect($unassignedTicketIds)->chunk(100)->each(function($ticketIds) use ($zendesk) {
-
-            // $unnasignedTicketsByTicketId = $tickets->keyBy('zendesk_ticket_id');
+            $unassignableTickets = collect($tickets)->unassignableTickets();
             
-            // $tickets = $zendesk->getTicketsByIds($tickets->pluck('zendesk_ticket_id')->all());
-            $tickets = $zendesk->getTicketsByIds($ticketIds->all());
-      
+            $unavailableTickets = $ticketIds->diff(collect($tickets)->pluck('id'));
 
-            foreach ($tickets as $i => $ticket) {
-                $type = Str::upper("already_" . $ticket->status);
-
-                if (in_array($ticket->status, ["new", "open", "pending"])) {
-                    $type = Agent::UNASSIGNMENT;
-                    
-                    $zendesk->updateTicket($ticket->id, [
-                        "custom_fields" => [
-                            [
-                            "id" => env("ZENDESK_AGENT_NAMES_FIELD", 360000282796),
-                            "value" => null
-                            ]
-                        ],
-                        "tags" => array_merge($ticket->tags, ["bagirata_agent_unavailable"]),
-                        "comment" =>  [
-                            "body" => "BAGIRATA Agent Unavailable: " . $this->agent->fullName,
-                            "author_id" => $this->agent->zendesk_agent_id,
-                            "public" => false
-                        ]
-                    ]);
-                }
-                Redis::srem('agent:'.$this->agent->id.':assignedTickets', $ticket->id);
-                $this->agent->assignments()->create([
-                    "type" => $type,
-                    "zendesk_view_id" => "TEMP_NO",
-                    "batch_id" => "TEMP_NO",
-                    "agent_id" => $this->agent->id,
-                    "agent_name" => $this->agent->fullName,
-                    "zendesk_ticket_id" => $ticket->id,
-                    "zendesk_ticket_subject" => $ticket->subject,
-                    "group_id" => $this->agent->zendesk_group_id,
-                    "response_status" => 200
-                ]);
+            // Remove unavailable tickets (ticket that might be deleted in Zendesk)
+            if ($unavailableTicketsCount = Redis::srem(sprintf("agent:%s:assignedTickets", $this->agent->id), ...$unavailableTickets->values()->all())) {
+                logs()->debug($unavailableTicketsCount);
             }
-    
+            
+            $response = $zendesk->unassignTickets($unassignableTickets->pluck('id')->values()->all(), $this->agent->zendesk_agent_id, $this->agent->fullName);
+
+            event(new UnassignmentsProcessed(optional($response)->job_status, $this->agent, $tickets));
         });
         
     }
