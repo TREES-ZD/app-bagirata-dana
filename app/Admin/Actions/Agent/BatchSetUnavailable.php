0839a4b132cc53e20c1dcd468f847f9bf55a578a
Janick Norman <janicknorman@gmail.com>
2020-07-10 15:50:37 +0700

    Using unassign after offline batch

--- app/Admin/Actions/Agent/BatchSetUnavailable.php
+++ app/Admin/Actions/Agent/BatchSetUnavailable.php
@@ -3,6 +3,7 @@
 namespace App\Admin\Actions\Agent;
 
 use App\Agent;
+use App\Jobs\Assignments\UnassignBatch;
 use Encore\Admin\Actions\BatchAction;
 use Illuminate\Database\Eloquent\Collection;
 
@@ -18,6 +19,10 @@ class BatchSetUnavailable extends BatchAction
             $agent->status = false;
             $agent->save();
         });
+
+        if ($agents->count() > 0) {
+            UnassignBatch::dispatch($agents)->onQueue('unassignment');
+        }
 
         return $this->response()->success('Success setting unavailable ' . $agents->count() . ' agent(s)')->refresh();
     }
