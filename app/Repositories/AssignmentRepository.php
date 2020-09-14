c59ab206bca49c5fde2a5f6e4a3db6b56f7634c6
Janick Norman <janicknorman@gmail.com>
2020-09-14 13:10:41 +0700

    QUICK FIX

--- app/Repositories/AssignmentRepository.php
+++ app/Repositories/AssignmentRepository.php
@@ -53,7 +53,7 @@ class AssignmentRepository
             $agents = $task->getAvailableAgents();
             $tickets = new TicketCollection($tickets->values()->all());
 
-            return $this->createAssignments($agents, $tickets, $batch, $previousFailedAssignments);
+            return $this->createAssignments($agents, $tickets, $batch, collect($previousFailedAssignments->all()));
         })->flatten();
     }
 
