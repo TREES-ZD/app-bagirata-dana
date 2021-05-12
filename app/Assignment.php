a6ef7abb1e0202ffb7ffbd3d6db7dda9d0ae543e
Janick Norman <janicknorman@gmail.com>
2021-05-12 10:00:03 +0700

    add Assignment constant

--- app/Assignment.php
+++ app/Assignment.php
@@ -7,6 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Assignment extends Model
 {
+    public const TYPE_ASSIGNMENT = "ASSIGNMENT";
+    public const TYPE_RETRIED_ASSIGNMENT = "RETRIED_ASSIGNMENT";
+    public const TYPE_REASSIGNMENT = "REASSIGNMENT";
+    public const TYPE_UNASSIGNMENT = "UNASSIGNMENT";
+    public const TYPE_OBSERVED_UNASSIGNMENT = "OBSERVED_UNASSIGNMENT";
+
+    public const RESPONSE_STATUS_SUCCESS = "200";
+    public const RESPONSE_STATUS_FAILED = "FAILED";
+    public const RESPONSE_STATUS_PENDING = "PENDING";
+
     protected $fillable = ["type", "zendesk_view_id", "batch_id", "agent_id", "agent_name", "zendesk_ticket_id", "zendesk_ticket_subject", "response_status", "created_at"];
 
     public function newCollection(array $models = [])
