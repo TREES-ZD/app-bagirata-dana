35c3c5f9f48622a513507d82b116fcfb188e0594
Janick Norman <janicknorman@gmail.com>
2021-05-12 11:00:43 +0700

    add RoundRobinEngine

--- app/Services/Assignments/RoundRobinEngine.php
+++ app/Services/Assignments/RoundRobinEngine.php
@@ -3,11 +3,13 @@
 namespace App\Services\Assignments;
 
 use App\Agent;
+use App\Assignment;
 use App\Services\Zendesk\Ticket;
-use App\Collections\AgentCollection;
-use App\Services\Zendesk\TicketCollection;
 use Illuminate\Support\Collection;
-use Zendesk\API\Resources\Core\TicketComments;
+use App\Collections\AgentCollection;
+use App\Collections\AssignmentCollection;
+use App\Services\Zendesk\TicketCollection;
+use App\Services\Assignments\PreparedAssignment;
 
 class RoundRobinEngine
 {
@@ -41,6 +43,11 @@ class RoundRobinEngine
      */
     public $assignmentPairs;
 
+    /**
+     * @var Collection $retriedAssignments
+     */
+    public $retriedAssignments;
+
     public function __construct()
     {
         $this->slots = collect();
@@ -48,10 +55,15 @@ class RoundRobinEngine
         $this->ticketsIndex = new TicketCollection();
         $this->agentOrders = collect();
         $this->assignmentPairs = collect();
+        $this->retriedAssignments = collect();
         $this->currentTime = now();
     }
 
-    public function make() {
+    /**
+     * @return Collection
+     */
+    public function make()
+    {
         $self = $this;
         return $this->assignmentPairs->map(function($pairs) use ($self) {
             $agent = $self->agents->firstWhere('id', $pairs[0]);
@@ -65,10 +77,20 @@ class RoundRobinEngine
                 'ticket_id' => $ticket->id,
                 'ticket_subject' => $ticket->subject,
                 "view_id" => $ticket->viewId(),
-                'type' => Agent::ASSIGNMENT,
+                'type' => Assignment::TYPE_ASSIGNMENT,
                 "batch" => "tes",
                 "created_at" => now()
             ];
+        });
+    }
+
+    public function makePreparedAssignments(): PreparedAssignmentCollection
+    {
+        $self = $this;
+        return (new PreparedAssignmentCollection($this->assignmentPairs->all()))->map(function($pairs) use ($self) {
+            $agent = $self->agents->firstWhere('id', $pairs[0]);
+            $ticket = $self->ticketsIndex->get($pairs[1]);
+            return (new PreparedAssignment($agent, $ticket,$ticket->viewId(), $pairs[2], $this->batch, now()));
         });
     }
 
@@ -85,11 +107,53 @@ class RoundRobinEngine
         $this->agents = $agents;
 
         // Build orders
-        $this->agents
-            ->groupByOrdersIdentifierTags()
-            ->each(function(AgentCollection $agents, $tag) use ($self) { 
-                $self->agentOrders->put($tag, $agents->getAssignmentOrders($tag));
-            });
+        // $this->agents
+        //     ->groupByOrdersIdentifierTags()
+        //     ->each(function(AgentCollection $agents, $tag) use ($self) { 
+        //         // $self->agentOrders->put($tag, $agents->getAssignmentOrders($tag));
+        //         $self->agentOrders->put($tag, $agents->pluck('id'));
+        //     });
+    }
+
+    public function setAgentOrders(Collection $agentOrders): RoundRobinEngine
+    {
+        $this->agentOrders = $agentOrders;
+
+        return $this;
+    }
+
+    public function setOrdersPerView(AssignmentCollection $latestAssignmentsPerView): RoundRobinEngine
+    {
+        $latestAssignmentsPerView = optional($latestAssignmentsPerView->sortBy('id'))->values() ?: AssignmentCollection::make();
+
+        $ordersByView = $latestAssignmentsPerView->mapToGroups(function(Assignment $assignment) {
+            return [$assignment->zendesk_view_id => $assignment->agent_id];
+        });
+        $defaultOrder = $latestAssignmentsPerView->mapWithKeys(function(Assignment $assignment) {
+            return [$assignment->agent_id => $assignment->id];
+        })->sort()->keys();
+
+        $newOrders = $this->agentOrders->filterMap(function($order, $tag) use ($ordersByView, $defaultOrder) {
+            $tag = (new OrderTag())->parseTag($tag);
+
+            $selectedOrder = $tag->viewId ? ($ordersByView->get($tag->viewId) ?: collect()) : $defaultOrder;
+ 
+            $agentIdsNotInAssignments = $order->diff($selectedOrder)->values();
+
+            return $agentIdsNotInAssignments->merge($selectedOrder);
+        });
+
+        $this->agentOrders = $this->agentOrders->merge($newOrders);
+
+        return $this;
+    }
+
+    public function assignWithPriority(TicketCollection $tickets): void
+    {
+        $recentlyFailedTickets = $tickets->whereIn('id', $this->retriedAssignments->keys()->all());
+        
+        $this->assign($recentlyFailedTickets);
+        $this->assign($tickets->prioritize());
     }
 
     public function assign(TicketCollection $tickets): void 
@@ -110,6 +174,11 @@ class RoundRobinEngine
         if (!$ticket->isAssignable()) {
             return false;
         }
+
+        if ($agent = $this->isRetriableTicket($ticket)) {
+            $this->recordAssignment($agent, $ticket, Assignment::TYPE_RETRIED_ASSIGNMENT);
+            return true;
+        }
         
         $agent = $this->chooseEligibleAgent($ticket);
         if ($this->checkAlreadyAssigned($ticket) || !$agent) {
@@ -121,6 +190,14 @@ class RoundRobinEngine
         $this->recordAssignment($agent, $ticket);
 
         return true;
+    }
+
+    public function addRetriedAssignments(AssignmentCollection $assignments): void
+    {
+        $self = $this;
+        $assignments->each(function(Assignment $assignment) use ($self) {
+            $self->retriedAssignments->put($assignment->zendesk_ticket_id, $assignment->agent_id);
+        });
     }
 
     public function chooseEligibleAgent(Ticket $ticket): ?Agent
@@ -137,7 +214,7 @@ class RoundRobinEngine
         return $agentOrderNum && $agent ? $agent : null;
     }
 
-    public function checkAlreadyAssigned(Ticket $ticket)
+    public function checkAlreadyAssigned(Ticket $ticket): bool
     {
         return $this->assignmentPairs
                 ->contains(function($assignmentPair) use ($ticket) {
@@ -162,8 +239,17 @@ class RoundRobinEngine
         return collect();
     }
 
-    protected function recordAssignment(Agent $agent, Ticket $ticket)
+    public function isRetriableTicket(Ticket $ticket): ?Agent
     {
-        $this->assignmentPairs->add([$agent->id, $ticket->id()]);
+        if ($agentId = $this->retriedAssignments->get($ticket->id)) {
+            return $this->agents->firstWhere('id', $agentId);
+        }
+
+        return null;
+    }
+
+    public function recordAssignment(Agent $agent, Ticket $ticket, $type = Assignment::TYPE_ASSIGNMENT): void
+    {
+        $this->assignmentPairs->add([$agent->id, $ticket->id(), $type]);
     }
 }
--- tests/Unit/Services/Assignments/RoundRobinEngineTest.php
+++ tests/Unit/Services/Assignments/RoundRobinEngineTest.php
@@ -2,14 +2,15 @@
 
 namespace Tests\Unit\Services\Assignments;
 
-use Mockery;
 use App\Agent;
+use App\Assignment;
 use App\Collections\AgentCollection;
-use App\Services\Agents\OrderTag;
+use App\Collections\AssignmentCollection;
+use App\Services\Assignments\OrderTag;
 use Tests\TestCase;
 use App\Services\Assignments\RoundRobinEngine;
 use App\Services\Zendesk\Ticket;
-use Tests\Helper\Factory\TicketFactory;
+use Tests\Helper\TicketFactory;
 
 class RoundRobinEngineTest extends TestCase
 {
@@ -17,71 +18,168 @@ class RoundRobinEngineTest extends TestCase
         parent::setUp();
     }
     
-    /**
-     * @method App\Repositories\AssignmentRepository::getAssignmentOrders
-     *
-     * @return void
-     */
-    public function test_AddAssignableAgents_membuatOrderanPemabgianAgentnya() {   
-        //  Given
-        $viewOneAgentsMock = $this->createMock(AgentCollection::class);
-        $viewOneAgentsMock->expects($this->atLeastOnce())
-                            ->method('getAssignmentOrders')
-                            ->willReturn(collect([2, 1, 3]));
-
-        $viewTwoAgentsMock = $this->createMock(AgentCollection::class);
-        $viewTwoAgentsMock->expects($this->atLeastOnce())
-                            ->method('getAssignmentOrders')
-                            ->willReturn(collect([5, 10, 7]));
-
-        /** @var \App\Collections\AgentCollection&\PHPUnit\Framework\MockObject\MockObject $agentCollectionMock */
-        $agentCollectionMock = $this->createMock(AgentCollection::class);
-        $agentCollectionMock->method('groupByOrdersIdentifierTags')
-                            ->willReturn(new AgentCollection([
-                                            "ordertag1" => $viewOneAgentsMock,
-                                            "ordertag2" => $viewTwoAgentsMock
-                                        ]));
-        
+    public function test_setOrdersPerView_akanMereorderAgentOrderBerdasarkanOrderPerAssignmentPerView()
+    {
+        $assignments = AssignmentCollection::make([
+            factory(Assignment::class)->make(['id' => 1, 'agent_id' => 20, 'zendesk_view_id' => 'view1']),
+            factory(Assignment::class)->make(['id' => 2, 'agent_id' => 30, 'zendesk_view_id' => 'view2']),
+            factory(Assignment::class)->make(['id' => 3, 'agent_id' => 10, 'zendesk_view_id' => 'view1']),
+            factory(Assignment::class)->make(['id' => 4, 'agent_id' => 30, 'zendesk_view_id' => 'view1']),
+            factory(Assignment::class)->make(['id' => 5, 'agent_id' => 10, 'zendesk_view_id' => 'view2']),
+            factory(Assignment::class)->make(['id' => 6, 'agent_id' => 20, 'zendesk_view_id' => 'view2'])
+        ]);
         $rre = new RoundRobinEngine();
-        $rre->addAssignableAgents($agentCollectionMock);
-        
-        $this->assertCount(2, $rre->agentOrders);
-        $this->assertSame($rre->getAgentOrders("ordertag1")->all(), [2, 1, 3]);
-        $this->assertSame($rre->getAgentOrders("ordertag2")->all(), [5, 10, 7]);
-        $this->assertSame($rre->getAgentOrders("ordertagX")->all(), []);
+        $rre->agentOrders = collect([
+            $tagViewOne = (string) (new OrderTag("view1", "group1")) => collect([10, 20, 30]),
+            $tagViewTwo = (string) (new OrderTag("view2", "group2")) => collect([10, 20, 30])
+        ]);
+
+        $rre->setOrdersPerView($assignments);
+
+        $this->assertSame([20, 10, 30], $rre->agentOrders[$tagViewOne]->all());
+        $this->assertSame([30, 10, 20], $rre->agentOrders[$tagViewTwo]->all());
     }
 
-    public function test_AssignTicket_hanyaAkanMengassignTicketYangSamaSekali() 
-    {  
-        $ticketOne = new Ticket((object) ["id" => 1, "status" => "open"]);
-        $ticketTwo = new Ticket((object) ["id" => 2, "status" => "open"]);
+    public function test_setOrdersPerView_akanMemprioritaskanDanMembiarkanOrderAgentIdJikaTidakAdaDiAssignment()
+    {
+        $assignments = AssignmentCollection::make([
+            factory(Assignment::class)->make(['id' => 1, 'agent_id' => 20, 'zendesk_view_id' => 'view1']),
+            factory(Assignment::class)->make(['id' => 2, 'agent_id' => 10, 'zendesk_view_id' => 'view1']),
+            factory(Assignment::class)->make(['id' => 3, 'agent_id' => 30, 'zendesk_view_id' => 'view1']),
+        ]);
+        $rre = new RoundRobinEngine();
+        $rre->agentOrders = collect([
+            $tagViewOne = (string) (new OrderTag("view1", null)) => collect([10, 111, 112, 20, 30]),
+            $tagViewTwo = (string) (new OrderTag("view2", null)) => collect([111, 112])
+        ]);
+
+        $rre->setOrdersPerView($assignments);
+        
+        $this->assertSame([111, 112, 20, 10, 30], $rre->agentOrders[$tagViewOne]->all());
+        $this->assertSame([111, 112], $rre->agentOrders[$tagViewTwo]->all());
+    }
+
+    public function test_setOrdersPerView_akanMereorderTagTanpaViewDenganAssignmentTerLatest()
+    {
+        $assignments = AssignmentCollection::make([
+            factory(Assignment::class)->make(['id' => 1, 'agent_id' => 10, 'zendesk_view_id' => 'view1']),
+            factory(Assignment::class)->make(['id' => 2, 'agent_id' => 20, 'zendesk_view_id' => 'view1']),
+            factory(Assignment::class)->make(['id' => 3, 'agent_id' => 30, 'zendesk_view_id' => 'view1']),
+            factory(Assignment::class)->make(['id' => 4, 'agent_id' => 20, 'zendesk_view_id' => 'viewX']),
+            factory(Assignment::class)->make(['id' => 5, 'agent_id' => 10, 'zendesk_view_id' => 'viewX'])
+        ]);
+
+        $rre = new RoundRobinEngine();
+        $rre->agentOrders = collect([
+            $tagNoView =(string)  (new OrderTag(null, "group1")) => collect([10, 20, 30]),
+            $tagNoViewTwo =(string)  (new OrderTag(null, "group2")) => collect([20, 10, 30]),
+        ]);
+
+        $rre->setOrdersPerView($assignments);
+
+        $this->assertSame([30, 20, 10], $rre->agentOrders[$tagNoView]->all());
+        $this->assertSame([30, 20, 10], $rre->agentOrders[$tagNoViewTwo]->all());
+    }
+
+    public function test_assignTicket_hanyaAkanMencatatAssignmentJikaTiketKetemuAgentPilihannya()
+    {
+        // Arrange
+        list($ticketOne, $ticketTwo, $ticketThree) = app(TicketFactory::class)->unassigned()->makeMany(3);
+        
         /** @var \App\Services\Assignments\RoundRobinEngine&\PHPUnit\Framework\MockObject\MockObject $rre */
         $rre = $this->getMockBuilder(RoundRobinEngine::class)
                     ->setMethodsExcept(['assignTicket'])
                     ->getMock();
+        
+        // Assert
+        $rre->expects($this->exactly(3))
+        ->method('chooseEligibleAgent')
+        ->withConsecutive(
+            [$this->identicalTo($ticketOne)], 
+            [$this->identicalTo($ticketTwo)], 
+            [$this->identicalTo($ticketThree)])
+        ->willReturn(
+            $agentOne = factory(Agent::class)->make(["id" => 11]), 
+            null, 
+            $agentThree = factory(Agent::class)->make(["id" => 13])
+        );
+        $rre->expects($this->exactly(2))
+        ->method('recordAssignment')
+        ->withConsecutive(
+            [$this->identicalTo($agentOne), $this->identicalTo($ticketOne)],
+            [$this->identicalTo($agentThree), $this->identicalTo($ticketThree)],
+        );
 
-        $rre->expects($this->exactly(3))->method('checkAlreadyAssigned')->willReturn(false, true, false);
-        $rre->expects($this->exactly(3))->method('chooseEligibleAgent')->willReturn(
-                                                                            $agentOne = factory(Agent::class)->make(["id" => 11]), 
-                                                                            $agentTwo = factory(Agent::class)->make(["id" => 12]), 
-                                                                            $agentThree = factory(Agent::class)->make(["id" => 13])
-                                                                        );
-        $rre->expects($this->exactly(2))->method('rotateAgentOrders')->withConsecutive(
-                                                                        [$this->identicalTo($ticketOne)],
-                                                                        [$this->identicalTo($ticketTwo)]
-                                                                    );
-        
-        $resultOne = $rre->assignTicket($ticketOne);
-        $resultTwo = $rre->assignTicket($ticketOne);
-        $resultThree = $rre->assignTicket($ticketTwo);
-        
-        $this->assertTrue($resultOne);
-        $this->assertFalse($resultTwo);
-        $this->assertTrue($resultThree);
-        $this->assertCount(2, $rre->assignmentPairs, "assignment pairs are build after two successfull assignment"); 
-        $this->assertEquals([$agentOne->id, $ticketOne->id], $rre->assignmentPairs[0]);
-        $this->assertEquals([$agentThree->id, $ticketTwo->id], $rre->assignmentPairs[1]);
+        // Act
+        $rre->assignTicket($ticketOne);
+        $rre->assignTicket($ticketTwo);
+        $rre->assignTicket($ticketThree);
     }
+
+    public function test_AssignTicket_hanyaAkanMengassignTiketJikaTiketBelumTerassignSebelumnya() 
+    {  
+        // Arrange
+        list($ticketOne, $ticketTwo, $ticketThree) = app(TicketFactory::class)->unassigned()->makeMany(3);
+
+        /** @var \App\Services\Assignments\RoundRobinEngine&\PHPUnit\Framework\MockObject\MockObject $rre */
+        $rre = $this->getMockBuilder(RoundRobinEngine::class)
+                    ->setMethodsExcept(['assignTicket'])
+                    ->getMock();
+        $rre->method('isRetriableTicket')->willReturn(null);
+        $rre->method('chooseEligibleAgent')->willReturn($agentOne = factory(Agent::class)->make(["id" => 11]));
+        
+        // Assert
+        $rre->expects($this->exactly(3))
+            ->method('checkAlreadyAssigned')
+            ->withConsecutive(
+                [$this->identicalTo($ticketOne)], 
+                [$this->identicalTo($ticketTwo)], 
+                [$this->identicalTo($ticketThree)])
+            ->willReturnOnConsecutiveCalls(false, true, false);                                                                        
+        $rre->expects($this->exactly(2))
+            ->method('recordAssignment')
+            ->withConsecutive(
+                [$this->identicalTo($agentOne), $this->identicalTo($ticketOne)],
+                [$this->identicalTo($agentOne), $this->identicalTo($ticketThree)],
+            );
+        
+        // Act
+        $rre->assignTicket($ticketOne);
+        $rre->assignTicket($ticketTwo);
+        $rre->assignTicket($ticketThree);
+    }
+
+    public function test_assignTicket_akanMengassignRetriedAssignmentTanpaMerotateOrders()
+    {
+        // Arrange
+        /** @var \App\Services\Zendesk\Ticket&\PHPUnit\Framework\MockObject\MockObject $retriableTicket */
+        $retriableTicket = $this->createMock(Ticket::class);
+        $retriableTicket->method('isAssignable')->willReturn(true);
+        /** @var \App\Services\Zendesk\Ticket&\PHPUnit\Framework\MockObject\MockObject $notRetriableTicket */
+        $notRetriableTicket = $this->createMock(Ticket::class);
+        $notRetriableTicket->method('isAssignable')->willReturn(true);
+
+        /** @var \App\Services\Assignments\RoundRobinEngine&\PHPUnit\Framework\MockObject\MockObject $rre */
+        $rre = $this->getMockBuilder(RoundRobinEngine::class)->setMethodsExcept(['assignTicket'])->getMock();
+        $rre->method('chooseEligibleAgent')->willReturn($eligibleAgent = factory(Agent::class)->make());
+
+        //Assert
+        $rre->expects($this->exactly(2))
+            ->method('isRetriableTicket')
+            ->willReturnOnConsecutiveCalls($matchedRetriedAgent = factory(Agent::class)->make(), null);
+        $rre->expects($this->atLeastOnce())
+            ->method('recordAssignment')
+            ->withConsecutive(
+                [$this->identicalTo($matchedRetriedAgent), $this->identicalTo($retriableTicket), $this->matches(Agent::RETRIED_ASSIGNMENT)]
+            );
+        $rre->expects($this->once())
+            ->method('rotateAgentOrders')
+            ->with($this->identicalTo($notRetriableTicket));
+
+        // Act
+        $rre->assignTicket($retriableTicket);
+        $rre->assignTicket($notRetriableTicket);
+    }   
 
     /**
      * @testWith
@@ -90,7 +188,7 @@ class RoundRobinEngineTest extends TestCase
      *        ["ordertag1", [13, 12, 100], 13]
      *        ["ordertag1rotate", [11, 12, 13], 12]
      */
-    public function test_chooseEligibleAgent_WillGetAgentFromTheFirstOrderOfTicketTag(string $orderTag, array $order, int $expectedOutput)
+    public function test_chooseEligibleAgent_akanMereturnAgentPertamaDariOrderTagTiketnya(string $orderTag, array $order, int $expectedOutput)
     {
         //Arrange
         /** @var \App\Services\Zendesk\Ticket&\PHPUnit\Framework\MockObject\MockObject $ticketOne*/
@@ -124,17 +222,19 @@ class RoundRobinEngineTest extends TestCase
 
     public function test_ChooseEligibleAgent_WillReturnAgentWithoutViewIfNoViewFound()
     {
-        $t = $this->createPartialMock(Ticket::class, ['getOrderIdentifier']);
-
+        /** @var \App\Services\Zendesk\Ticket&\PHPUnit\Framework\MockObject\MockObject $ticketFromViewWithoutAgentsRegistered */
         $ticketFromViewWithoutAgentsRegistered = $this->createPartialMock(Ticket::class, ['getOrderIdentifier']);
         $ticketFromViewWithoutAgentsRegistered->ticket = (object) ["id" => 1];
         $ticketFromViewWithoutAgentsRegistered->method('getOrderIdentifier')->willReturn((new OrderTag())->setName("orderWithoutAgentsRegistered"));
         $ticketFromViewWithoutAgentsRegistered->ticket = (object) ["id" => 2];
+        /** @var \App\Services\Zendesk\Ticket&\PHPUnit\Framework\MockObject\MockObject $ticketFromViewNotExist */
         $ticketFromViewNotExist = $this->createPartialMock(Ticket::class, ['getOrderIdentifier']);
         $ticketFromViewNotExist->method('getOrderIdentifier')->willReturn((new OrderTag())->setName("orderNotExist"));
         $ticketFromViewNotExist->ticket = (object) ["id" => 3];
+        /** @var \App\Services\Zendesk\Ticket&\PHPUnit\Framework\MockObject\MockObject $ticketWithoutView */
         $ticketWithoutView = $this->createPartialMock(Ticket::class, ['getOrderIdentifier']);
         $ticketWithoutView->ticket = (object) ["id" => 4];
+        /** @var \App\Services\Zendesk\Ticket&\PHPUnit\Framework\MockObject\MockObject $successTicket */
         $successTicket = $this->createPartialMock(Ticket::class, ['getOrderIdentifier']);
         $successTicket->method('getOrderIdentifier')->willReturn((new OrderTag())->setName("order1"));
         $successTicket->ticket = (object) ["id" => 5];
@@ -162,5 +262,54 @@ class RoundRobinEngineTest extends TestCase
         $this->assertNull($b);
         $this->assertNull($c);
         $this->assertSame(12, $d->id);
+    }
+
+    public function test_addRetriedAssignment_akanMendaftarTicketYangAkanDiAssignKeSpesifikAgent()
+    {
+        $assignmentOne = factory(Assignment::class)->make(["agent_id" => 1, "zendesk_ticket_id" => 11]);
+        $assignmentTwo = factory(Assignment::class)->make(["agent_id" => 2, "zendesk_ticket_id" => 12]);
+        $assignmentThree = factory(Assignment::class)->make(["agent_id" => 3, "zendesk_ticket_id" => 11]);
+        $assignments = AssignmentCollection::make([$assignmentOne, $assignmentTwo]);
+        // $ticket = app(TicketFactory::class)->id(12)->make();
+
+        $rre = new RoundRobinEngine();
+        $rre->addRetriedAssignments($assignments);
+        
+        $this->assertSame(1, $rre->retriedAssignments->get(11));
+        $this->assertSame(2, $rre->retriedAssignments->get(12));
+        $this->assertTrue($rre->retriedAssignments->contains(1));
+        $this->assertTrue($rre->retriedAssignments->contains(2));
+        $this->assertFalse($rre->retriedAssignments->contains(3));
+    }
+
+    /**
+     * @testWith
+     *        [10, 1]
+     *        [11, 2]
+     *        [12, 3]
+     */
+    public function test_isRetriableTicket_hanyaAkanMereturnTicketDanAgentYangSudahTerdaftar($id, $resultAgentId)
+    {
+        $ticket = app(TicketFactory::class)->id($id)->make();
+        $ticketNotRetriable = app(TicketFactory::class)->id(100)->make();
+        $ticketRetriedNoAgent = app(TicketFactory::class)->id(14)->make();
+
+        $rre = new RoundRobinEngine();
+        $rre->retriedAssignments = collect([10 => 1, 11 => 2, 12 => 3, 14 => 5]);
+        $rre->agents = AgentCollection::make([
+            factory(Agent::class)->make(["id" => 1]), 
+            factory(Agent::class)->make(["id" => 2]), 
+            factory(Agent::class)->make(["id" => 3]), 
+            factory(Agent::class)->make(["id" => 4])
+        ]);
+        
+        $agentResult = $rre->isRetriableTicket($ticket);
+        $ticketNotRetriedAgentRegisteredResult = $rre->isRetriableTicket($ticketNotRetriable);
+        $ticketRetriedButNoAgentResult = $rre->isRetriableTicket($ticketRetriedNoAgent);
+
+        $this->assertInstanceOf(Agent::class, $agentResult);
+        $this->assertSame($resultAgentId, $agentResult->id);
+        $this->assertNull($ticketNotRetriedAgentRegisteredResult);
+        $this->assertNull($ticketRetriedButNoAgentResult);
     }
 }
