50170530ae476c7ca9b420952bdb4c98c14adf8f
JanickNorman <janicknorman@gmail.com>
2023-03-31 14:16:47 +0700

    fix serialize date after laravel 9 bump

--- app/Models/Assignment.php
+++ app/Models/Assignment.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use DateTimeInterface;
 
 class Assignment extends Model
 {
-    protected $fillable = ["type", "zendesk_view_id", "batch_id", "agent_id", "agent_name", "zendesk_ticket_id", "zendesk_ticket_subject", "response_status", "created_at"];
+    protected $fillable = ["type", "zendesk_view_id", "batch_id", "agent_id", "agent_name", "zendesk_ticket_id", "zendesk_ticket_subject", "response_status", "created_at"];    
+    /**
+     * Prepare a date for array / JSON serialization.
+     *
+     * @param  \DateTimeInterface  $date
+     * @return string
+     */
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
 }
--- app/Models/AvailabilityLog.php
+++ app/Models/AvailabilityLog.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use DateTimeInterface;
 
 class AvailabilityLog extends Model
 {
@@ -10,4 +11,16 @@ class AvailabilityLog extends Model
     const UNAVAILABLE = "Unavailable";
     
     protected $guarded = ['id'];
+
+     
+    /**
+     * Prepare a date for array / JSON serialization.
+     *
+     * @param  \DateTimeInterface  $date
+     * @return string
+     */
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
 }
