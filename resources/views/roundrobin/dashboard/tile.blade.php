5e5423dbb6e69a7ac293628d36266061892f1ece
Janick Norman <janicknorman@gmail.com>
2023-05-23 01:31:27 +0700

    round 1 decimal tile

--- resources/views/roundrobin/dashboard/tile.blade.php
+++ resources/views/roundrobin/dashboard/tile.blade.php
@@ -2,7 +2,7 @@
     
     <a style="font-size: medium" class="label label-success">{{$data}}</a>
         <a style="font-size: medium" class="label label-danger">{{$failed_data}}</a>
-        <span style="font-size: larger"> ({{($data + $failed_data) ? 100*($failed_data / ($data + $failed_data)) : 0}}% fail rate)</span>
+        <span style="font-size: larger"> ({{($data + $failed_data) ? round(100*($failed_data / ($data + $failed_data))) : 0}}% fail rate)</span>
 
    
 </div>
