8c9d1e64cf1cc7f1711b92c74443bf4f712641c3
Janick Norman <janicknorman@gmail.com>
2023-05-28 12:52:15 +0700

    tambah columns availlogs dan assignments

---
+++ database/migrations/2023_05_28_123614_add_zendesk_ticket_status_and_assigned_at_columns_to_assignments.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('assignments', function (Blueprint $table) {
+            $table->string('zendesk_ticket_status', 60)->nullable();
+            $table->timestamp('assigned_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('assignments', function (Blueprint $table) {
+            $table->dropColumn(['zendesk_ticket_status', 'assigned_at']);
+        });
+    }
+};
+
