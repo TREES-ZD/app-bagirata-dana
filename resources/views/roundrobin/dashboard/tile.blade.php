5c243db04801ec9a5f1a09ab7290e44768443675
Janick Norman <janicknorman@gmail.com>
2023-06-11 15:38:52 +0700

    update tile

--- resources/views/roundrobin/dashboard/tile.blade.php
+++ resources/views/roundrobin/dashboard/tile.blade.php
@@ -1,7 +1,7 @@
 <div class="text-center">
     
     <a style="font-size: revert" class="label label-success">{{$data}}</a>
-    @if (isset($failed_data))
+    @if (isset($failed_data) && $failed_data != 0)
         <a style="font-size: revert" class="label label-danger">{{$failed_data}}</a>
         <span style="font-size: revert"> ({{($data + $failed_data) ? round(100*($failed_data / ($data + $failed_data))) : 0}}% fail rate)</span>        
     @endif
